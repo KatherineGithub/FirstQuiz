50d52a7ce385659b7410a16b8226d77c04b1b870
KatherineGithub <Katherine.cui@bigfoodie.co.uk>
2018-02-09 15:36:37 +0000

    versionCode4 versionName1.3

---
+++ app/src/main/java/com/bignerdranch/android/firstquiz/ItemShowActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.firstquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_show);
+    }
+}
